793e9f3ac28e14c90695ef495cc563329b0aedc6
jaehyeon <hyesung6516@naver.com>
2023-04-21 14:02:37 +0900

    230421 14:02

---
+++ src/views/Footer/index.tsx
@@ -0,0 +1,10 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+export default function index() {
+  return (
+    <Box sx={{ 'width': '100%', 'height': "150px" , 'backgroundColor' : "#666666", 'display' : 'flex' }}>
+        
+    </Box>
+  )
+}
