a3e195b92d1f33589f4b5bc7cffcc323465aea57
dlackdtjq123@naver.com <dlackdtjq123@naver.com>
2023-05-02 17:07:27 +0900

    test

--- src/views/Main/MainContent/MainLeftContent/index.tsx
+++ src/views/Main/MainContent/MainLeftContent/index.tsx
@@ -1,10 +1,31 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material'
+import React, { useState } from 'react'
 
-export default function index() {
+export default function MainLeftContent() {
+  const [age, setAge] = useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
   return (
     <Box sx={{ width: '55%', height: '100%', mr:'5%', backgroundColor:'beige' }}>
-        {/* <Box sx={{width: '200px', height:'200px', backgroundColor:'red'}}></Box> */}
+      <Box sx={{ p: '20px'}}>
+        <FormControl sx={{ width: '100px', height: '100px'}}>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={age}
+            label="Age"
+            onChange={handleChange}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
     </Box>
+    
   )
 }
