247fff97d6b363cd0ba7901c3ca73a09a792a0ef
KDW999 <hide4321@naver.com>
2023-05-04 17:34:52 +0900

    ReviewBoardWriteView

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -68,9 +68,9 @@ export const SIGN_UP_CHECKBOX_LIST: IPreviewFestivalItem[] = [
         festivalArea: '테스트지역',
         festivalCost: '무료',
         onelineReviewAverage: null,
-        festivalInformation:null,
-        festivalInformationUrl:null
-    },{
+        festivalInformation: null,
+        festivalInformationUrl: null
+    }, {
         festivalNumber: 1,
         festivalName: '테스트축제이름',
         festivalType: '테스트축제타입2',
@@ -80,9 +80,9 @@ export const SIGN_UP_CHECKBOX_LIST: IPreviewFestivalItem[] = [
         festivalArea: '테스트지역',
         festivalCost: '무료',
         onelineReviewAverage: null,
-        festivalInformation:null,
-        festivalInformationUrl:null
-    },{
+        festivalInformation: null,
+        festivalInformationUrl: null
+    }, {
         festivalNumber: 1,
         festivalName: '테스트축제이름',
         festivalType: '테스트축제타입3',
@@ -92,9 +92,9 @@ export const SIGN_UP_CHECKBOX_LIST: IPreviewFestivalItem[] = [
         festivalArea: '테스트지역',
         festivalCost: '무료',
         onelineReviewAverage: null,
-        festivalInformation:null,
-        festivalInformationUrl:null
-    },{
+        festivalInformation: null,
+        festivalInformationUrl: null
+    }, {
         festivalNumber: 1,
         festivalName: '테스트축제이름',
         festivalType: '테스트축제타입4',
@@ -104,9 +104,9 @@ export const SIGN_UP_CHECKBOX_LIST: IPreviewFestivalItem[] = [
         festivalArea: '테스트지역',
         festivalCost: '무료',
         onelineReviewAverage: null,
-        festivalInformation:null,
-        festivalInformationUrl:null
-    },{
+        festivalInformation: null,
+        festivalInformationUrl: null
+    }, {
         festivalNumber: 1,
         festivalName: '테스트축제이름',
         festivalType: '테스트축제타입5',
@@ -116,38 +116,38 @@ export const SIGN_UP_CHECKBOX_LIST: IPreviewFestivalItem[] = [
         festivalArea: '테스트지역',
         festivalCost: '무료',
         onelineReviewAverage: null,
-        festivalInformation:null,
-        festivalInformationUrl:null
+        festivalInformation: null,
+        festivalInformationUrl: null
     },
-    
+
 ];
-export const FESTIVALLIST:IPreviewFestivalItem[]=
-[
-    {
-        festivalNumber:1,
-        festivalName:"빙어축제",
-        festivalType:"얼음낚시축제",
-        festivalDurationStart: "2023-01-01",
-        festivalDurationEnd:"2023-01-15",
-        festivalTime:"08:00~18:00",
-        festivalArea:"광주",
-        festivalCost:"무료",
-        onelineReviewAverage: 10,
-        festivalInformationUrl:"https://search.pstatic.net/common?type=n&size=174x250&quality=85&direct=true&src=https%3A%2F%2Fcsearch-phinf.pstatic.net%2F20221222_86%2F1671684709584YyuPG_JPEG%2F2937394_image2_1.jpg",
-        festivalInformation:"대자연과 함께하는 '인제빙어축제' 인제 빙어축제는 자연환경이 선물해 준 광활한 벌판 속에서 빙어를 잡고, 겨울 놀이터에서 다함께 즐기며 하나되는 축제이다.1997년 1회를 시작으로 관광객들의 많은 관심과 참여속에서 점점 더 발전하고 변화하고 있는 인제빙어축제는 시대에 발맞춰 여러가지 프로그램들을 개발하고 있다. 천혜의 자연을 배경으로 겨울철 소양강 최상류로 찾아드는 빙어떼의 아름다운 귀환과 즐거운 추억을 인제빙어축제와 함께 만들어 가시기 바란다."
-    },
-    {
-        festivalNumber:1,
-        festivalName:"빙어축제",
-        festivalType:"얼음낚시축제",
-        festivalDurationStart: "2023-01-01",
-        festivalDurationEnd:"2023-01-15",
-        festivalTime:"08:00~18:00",
-        festivalArea:"광주",
-        festivalCost:"무료",
-        onelineReviewAverage: 10,
-        festivalInformationUrl:"https://search.pstatic.net/common?type=n&size=174x250&quality=85&direct=true&src=https%3A%2F%2Fcsearch-phinf.pstatic.net%2F20230419_187%2F1681867631467bIKbx_JPEG%2F2975155_image2_1.jpg",
-        festivalInformation:"대자연과 함께하는 '인제빙어축제' 인제 빙어축제는 자연환경이 선물해 준 광활한 벌판 속에서 빙어를 잡고, 겨울 놀이터에서 다함께 즐기며 하나되는 축제이다.1997년 1회를 시작으로 관광객들의 많은 관심과 참여속에서 점점 더 발전하고 변화하고 있는 인제빙어축제는 시대에 발맞춰 여러가지 프로그램들을 개발하고 있다. 천혜의 자연을 배경으로 겨울철 소양강 최상류로 찾아드는 빙어떼의 아름다운 귀환과 즐거운 추억을 인제빙어축제와 함께 만들어 가시기 바란다."
-    }
-    ,
-]
+export const FESTIVALLIST: IPreviewFestivalItem[] =
+    [
+        {
+            festivalNumber: 1,
+            festivalName: "빙어축제",
+            festivalType: "얼음낚시축제",
+            festivalDurationStart: "2023-01-01",
+            festivalDurationEnd: "2023-01-15",
+            festivalTime: "08:00~18:00",
+            festivalArea: "광주",
+            festivalCost: "무료",
+            onelineReviewAverage: 10,
+            festivalInformationUrl: "https://search.pstatic.net/common?type=n&size=174x250&quality=85&direct=true&src=https%3A%2F%2Fcsearch-phinf.pstatic.net%2F20221222_86%2F1671684709584YyuPG_JPEG%2F2937394_image2_1.jpg",
+            festivalInformation: "대자연과 함께하는 '인제빙어축제' 인제 빙어축제는 자연환경이 선물해 준 광활한 벌판 속에서 빙어를 잡고, 겨울 놀이터에서 다함께 즐기며 하나되는 축제이다.1997년 1회를 시작으로 관광객들의 많은 관심과 참여속에서 점점 더 발전하고 변화하고 있는 인제빙어축제는 시대에 발맞춰 여러가지 프로그램들을 개발하고 있다. 천혜의 자연을 배경으로 겨울철 소양강 최상류로 찾아드는 빙어떼의 아름다운 귀환과 즐거운 추억을 인제빙어축제와 함께 만들어 가시기 바란다."
+        },
+        {
+            festivalNumber: 1,
+            festivalName: "빙어축제",
+            festivalType: "얼음낚시축제",
+            festivalDurationStart: "2023-01-01",
+            festivalDurationEnd: "2023-01-15",
+            festivalTime: "08:00~18:00",
+            festivalArea: "광주",
+            festivalCost: "무료",
+            onelineReviewAverage: 10,
+            festivalInformationUrl: "https://search.pstatic.net/common?type=n&size=174x250&quality=85&direct=true&src=https%3A%2F%2Fcsearch-phinf.pstatic.net%2F20230419_187%2F1681867631467bIKbx_JPEG%2F2975155_image2_1.jpg",
+            festivalInformation: "대자연과 함께하는 '인제빙어축제' 인제 빙어축제는 자연환경이 선물해 준 광활한 벌판 속에서 빙어를 잡고, 겨울 놀이터에서 다함께 즐기며 하나되는 축제이다.1997년 1회를 시작으로 관광객들의 많은 관심과 참여속에서 점점 더 발전하고 변화하고 있는 인제빙어축제는 시대에 발맞춰 여러가지 프로그램들을 개발하고 있다. 천혜의 자연을 배경으로 겨울철 소양강 최상류로 찾아드는 빙어떼의 아름다운 귀환과 즐거운 추억을 인제빙어축제와 함께 만들어 가시기 바란다."
+        }
+        ,
+    ]
