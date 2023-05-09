f7af383c52aa6ae1b5bd92e17e12f5248b74edff
jaehyeon <hyesung6516@naver.com>
2023-05-09 16:31:19 +0900

    3개 컴포넌트 상태 합치기

--- src/views/Main/MainContent/index.tsx
+++ src/views/Main/MainContent/index.tsx
@@ -12,12 +12,12 @@ export default function MainContent() {
       <Box sx={{ mt:'100px', width: '100%', height: 'auto', display:'flex', justifyContent: 'center'}}>
           <Box sx={{ width:'80vw', height:'auto', display: 'flex' }}>
               <MainLeftContent setClickPage={setClickPage} clickPage={clickPage}/>
-              <MainRightContent/>
+              <MainRightContent clickPage={clickPage}/>
           </Box>
       </Box>
       <Box sx={{ mt:'150px', width: '100%', display:'flex', justifyContent: 'center'}}>
           <Box sx={{width: '80vw', dsiplay:'flex'}}>
-              <FestivalBoard />
+              <FestivalBoard setClickPage={setClickPage} clickPage={clickPage}/>
           </Box>
       </Box>
     </>
