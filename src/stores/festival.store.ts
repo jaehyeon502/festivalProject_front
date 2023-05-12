0f3c06ae37d9f8ba22fba5cb44267e8a63d00246
dlackdtjq123@naver.com <dlackdtjq123@naver.com>
2023-05-12 13:54:44 +0900

    2023-05-12

--- src/stores/festival.store.ts
+++ src/stores/festival.store.ts
@@ -1,17 +1,13 @@
-
-import { IPreviewFestivalItem } from 'src/interfaces';
 import {create} from 'zustand';
 
 interface IPreviewFestivalSimpleListItemStore {
-    festival: IPreviewFestivalItem[] | null;
-    setFestival:(setFestivalInformationUrl:IPreviewFestivalItem[] | null)=>void;
+    festivalNumber: number | null;
+    setFestivalNumber:(festivalNumber: null)=>void;
 }
 
 const useStore=create<IPreviewFestivalSimpleListItemStore>((set)=>({
-    festival: [],
-    setFestival:(festival)=>set((state)=>({...state,festival}))
-
-
+    festivalNumber: null,
+    setFestivalNumber: (festivalNumber)=>set((state)=>({...state, festivalNumber}))
 }))
 
 export default useStore;
--- src/views/Main/MainContent/MainLeftContent/index.tsx
+++ src/views/Main/MainContent/MainLeftContent/index.tsx
@@ -1,5 +1,4 @@
-import { Box, Grid, IconButton, IconButtonProps, Pagination, SelectChangeEvent, styled } from '@mui/material'
-import axios, { AxiosResponse } from 'axios';
+import { Box, Grid, Pagination } from '@mui/material'
 import React from 'react';
 import {  useEffect, useState } from 'react'
 import FestivalSimpleListItem from 'src/components/FestivalSimpleListItem';
@@ -7,32 +6,16 @@ import MonthAndAreaButton from 'src/components/MonthAndAreaIButton';
 import { usePagingHook } from 'src/hooks';
 import { IPreviewFestivalSimpleListItem } from 'src/interfaces';
 import { SIMPLELIST } from 'src/mock';
-import { useFestivalStore } from 'src/stores';
 import { getpagecount } from 'src/utils';
 
 import FestivalOnclickChangeItem from 'src/components/FestivalOnclickChangeItem';
+import axios from 'axios';
 interface Props {
   clickPage: boolean;
   setClickPage: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-interface ExpandMoreProps extends IconButtonProps {
-  expand: boolean;
-}
-
-const ExpandMore = styled((props: ExpandMoreProps) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
-
 export default function MainLeftContent({ setClickPage, clickPage }: Props) {
-
 
   const { festivalList, viewList, pageNumber, onPageHandler, COUNT, setFestivalList } = usePagingHook(4);
   const [selectedFestival, setSelectedFestival] = useState<IPreviewFestivalSimpleListItem | null>(null);
@@ -44,6 +27,10 @@ export default function MainLeftContent({ setClickPage, clickPage }: Props) {
     setClickPage(true);
   }
 
+  const getFestivalNumber = (festivalNumber) => {
+    axios.get
+  }
+
   useEffect(() => {
     setFestivalList(SIMPLELIST);
   }, []);
@@ -51,6 +38,7 @@ export default function MainLeftContent({ setClickPage, clickPage }: Props) {
   return (
     //? 전체 테이블
     //? selectedFestival를 만들어서 true이면 실행
+    //? onClick이 아닌 setClickPage에 setClickPage를 넣어주고 item에는 selectedFestival을 넣어줬다(새로 만들지 않았어도 됐다.)
     <Box sx={{ width: '55%', height: '100%', mr: '5%', backgroundColor: '' }}>
       {clickPage && selectedFestival ? (
         <FestivalOnclickChangeItem setClickPage={setClickPage} item={selectedFestival} />
